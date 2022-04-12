e0cc220837be8a37524afa3cac98ae7b3c1024c7
AngelZapata582 <angelzapata582@gmail.com>
2022-04-12 15:01:01 -0500

    agrege rutas para los listados

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -1,15 +1,17 @@
 import {createWebHistory, createRouter} from 'vue-router';
-import main from '../components/Main.vue';
+import listproducts from '../components/Main.vue';
 import cg from '../components/componentG.vue';
 import pnf from '../components/PageNotFound.vue';
 import app from '../../App.vue';
 import login from '../components/Login-page.vue';
+import listuser from '../components/ListUser.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '', component:cg, children: [
             {path:'/:catchAll(.*)',component:pnf,name:'NotFound'},
-            {path: '', component:main, name:'home'}
+            {path: '/productos', component:listproducts, name:'listproducts'},
+            {path: '/usuarios', component:listuser, name:'listuser'}
         ]},
         {path: '/login', component:login, name:'login'}
     ]
