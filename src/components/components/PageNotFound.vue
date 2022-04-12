952bb7c8b1542ca151161df0ddd318735b61e6d7
AngelZapata582 <angelzapata582@gmail.com>
2022-04-12 15:00:43 -0500

    agrege pagina para 404

---
+++ src/components/components/PageNotFound.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container box mt-6" style="width:30%;">
+        <b class="mx-auto is-align-content-center is-size-1 has-text-primary">404! </b>
+        <b class="mx-auto is-align-content-center is-size-1 has-text-primary">Pagina no encontrada</b>
+    </div>
+</template>
