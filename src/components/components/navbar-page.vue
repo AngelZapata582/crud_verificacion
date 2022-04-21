feca89fd1196ebcf2e6a47b61322b8751ae87c35
AngelZapata582 <angelzapata582@gmail.com>
2022-04-21 02:23:57 -0500

    cambio en el menu de navbar

--- src/components/components/navbar-page.vue
+++ src/components/components/navbar-page.vue
@@ -1,18 +1,26 @@
 <template>
     <navbar class="navbar is-dark p-2" role="navigation">
         <div class="navbar-brand">
-            <img class="image is-48x48"  src="../../assets/logo.svg"/>
+            <img class="image is-48x48" src="../../assets/logo.svg" />
         </div>
         <div class="navbar-menu">
             <div class="navbar-end">
-                <div class="select is-primary mx-2 my-auto">
-                    <select v-model="selected">
-                        <option @click="click(selected)">productos</option>
-                        <option @click="click(selected)">usuarios</option>
-                    </select>
+                
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        Menu
+                    </a>
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item" href="/productos">
+                            Productos
+                        </a>
+                        <a class="navbar-item" href="/usuarios">
+                            Usuarios
+                        </a>
+                    </div>
                 </div>
                 <div class="mx-2 my-auto">
-                    Bienvenido {{user}}
+                    Bienvenido {{ user }}
                 </div>
             </div>
         </div>
@@ -21,15 +29,10 @@
 
 <script>
 export default {
-    data(){
-        return{
-            selected:'productos',
-            user:'user 3'
-        }
-    },
-    methods: {
-        click(v){
-            console.log(v)
+    data() {
+        return {
+            selected: 'productos',
+            user: 'user 3'
         }
     }
 }
