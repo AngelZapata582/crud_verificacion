962751c0d7b86261aead3149e985982bde22bc73
AngelZapata582 <angelzapata582@gmail.com>
2022-04-21 02:22:36 -0500

    agregacion de nuevas vistas para la gestion de usuarios

---
+++ src/components/components/EditUser.vue
@@ -0,0 +1,53 @@
+<template>
+    <form class="box m-auto" style="width:450px;">
+                    <div class="field">
+                        <label class="label">Nombre</label>
+                        <div class="control">
+                            <input v-model="user.Nombre" class="input" type="text" placeholder="Nombre" />
+                        </div>
+                    </div>
+                    
+                    <div class="field">
+                        <label class="label">Rol</label>
+                        <div class="control">
+                            <input v-model="user.Rol" class="input" type="number" placeholder="Rol" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <div class="control">
+                            <input v-model="user.email" class="input" type="text" placeholder="Email" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <button class="button is-info" @click="agregar()">Enviar</button>
+                    </div>
+                </form>
+</template>
+<script>
+import axios from "axios";
+export default {
+    mounted(){
+        this.getuser(this.$route.params.id)
+    },
+    props:{
+        valueid:0
+    },
+    data() {
+        return {
+            user:{}
+        };
+    },
+    methods:{
+        async getuser(id){
+            await axios.get('http://localhost:3333/user/'+id)
+            .then((response)=>{
+                this.user = response.data.user
+            })
+            .catch((error)=>console.log(error))
+        },
+        
+    }
+};
+
+</script>
---
+++ src/components/components/NewUser.vue
@@ -0,0 +1,63 @@
+<template>
+    <form @submit.prevent="agregar" class="box m-auto" style="width:450px;">
+        <div class="field">
+            <label class="label">Nombre</label>
+            <div class="control">
+                <input v-model="user.Nombre" class="input" type="text" placeholder="Nombre" />
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label">Rol</label>
+            <div class="control">
+                <input v-model="user.Rol" class="input" type="number" placeholder="Edad" />
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+                <input v-model="user.email" class="input" type="text" placeholder="Email" />
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+                <input v-model="user.password" class="input" type="password" placeholder="Password" />
+            </div>
+        </div>
+        <div class="field">
+            <button class="button is-info">Enviar</button>
+        </div>
+    </form>
+</template>
+<script>
+import axios from "axios";
+export default {
+    mounted() {
+        
+    },
+    data() {
+        return {
+            user: {
+                Nombre: '',
+                Apellido: '',
+                Rol: null,
+                email: '',
+                password: ''
+            },
+            users: []
+        };
+    },
+    methods: {
+        agregar() {
+            axios.post('http://localhost:3333/users',this.user)
+            .then(() => alert('usuario agregado'))
+            .catch((error) => console.log(error))
+        },
+        editar() {
+
+        }
+    }
+};
+
+</script>
